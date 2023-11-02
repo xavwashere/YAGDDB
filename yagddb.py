66403bb94fe3c662ee76bb6c0322f84112860fbd
Exyrus <97399129+Exyrus@users.noreply.github.com>
2023-11-02 12:48:40 +0000

    Added level searching

--- yagddb.py
+++ yagddb.py
@@ -16,6 +16,8 @@ from time import sleep
 
 import itertools
 
+import asyncio
+
 # get the starting time
 start = time.perf_counter()
 
@@ -65,6 +67,8 @@ async def get_owner(guild_id : int) -> discord.Member:
     guild = client.get_guild(guild_id)
     return guild.owner
 
+search_group = app_commands.Group(name="search", description="Search for something (users or levels)", guild_ids=[1155489454031654943])
+
 # uwu
 @t.command(name="ping", guild=discord.Object(id=1155489454031654943))
 async def ping(interaction):
@@ -79,15 +83,15 @@ async def daily(interaction):
     except:
         return await interaction.response.send_message("Daily level not found.")
     
-    # replace _ with whitespace because gd.py didn't do it for me (this is actually useless in the daily func but just in case ig)
-    dd_name = str.replace(d.difficulty.name, '_', ' ')
+    # replace _ with whitespace and pretty it up a lil
+    dd_name = str.replace(d.difficulty.name, '_', ' ').lower().capitalize()
     
     # create embed and send it
     e = (
         discord.Embed(colour=0x00C9FF)
         .add_field(name="Current Daily", value="{0} ({1})".format(d.name, d.id))
-        .add_field(name="Rating", value="{0}* ({1})".format(d.stars, dd_name))
-        .set_footer(text="Daily Creator: {0}".format(d.creator.name))
+        .add_field(name="Rating", value="{0}<:Star:1166360223859101737> ({1})".format(d.stars, dd_name))
+        .set_footer(icon_url="https://preview.redd.it/putting-my-geometry-dash-creator-points-image-here-so-v0-sgfl38xxycta1.png?width=640&crop=smart&auto=webp&s=817ca45d6616a201980b7be4fd980ec53e26f721", text=": {0}".format(d.creator.name))
     )
 
     await interaction.response.send_message(embed=e)
@@ -108,13 +112,13 @@ async def weekly(interaction):
     e = (
         discord.Embed(colour=0x00C9FF)
         .add_field(name="Current Weekly", value="{0} ({1})".format(w.name, w.id))
-        .add_field(name="Rating", value="{0}* ({1})".format(w.stars, wd_name))
-        .set_footer(text="Weekly Creator: {0}".format(w.creator.name))
+        .add_field(name="Rating", value="{0}<:Star:1166360223859101737> ({1})".format(w.stars, wd_name))
+        .set_footer(icon_url="https://preview.redd.it/putting-my-geometry-dash-creator-points-image-here-so-v0-sgfl38xxycta1.png?width=640&crop=smart&auto=webp&s=817ca45d6616a201980b7be4fd980ec53e26f721", text=": {0}".format(w.creator.name))
     )
 
     await interaction.response.send_message(embed=e)
 
-@t.command(name="search_users", description="Search for a user and display their stats", guild=discord.Object(id=1155489454031654943))
+@search_group.command(name="user", description="Search for a user and display their stats")
 async def search_user(interaction, user : str):
     try:
         search = await gd_client.search_user(user)
@@ -128,15 +132,45 @@ async def search_user(interaction, user : str):
 
         id = r.id
         name = r.name
+        downloads = r.downloads
 
     e = (
         discord.Embed(colour=0x00C9FF)
         .add_field(name="Name", value="{0} (ID: {1})".format(search.name, search.account_id))
-        .add_field(name="Stats", value="Stars: {0}\nDiamonds: {1}\nDemons: {2}\nSecret Coins: {3}\nUser Coins: {4}\nCreator Points: {5}".format(stats.stars, stats.diamonds, stats.demons, stats.secret_coins, stats.user_coins, stats.creator_points))
-        .add_field(name="Most Recent Level", value="{0} ({1})".format(name, id))
+        .add_field(name="Stats", value="<:Star:1166360223859101737>: {0}\n<:Diamond:1166362286496153690>: {1}\n<:Demon:1169589936505229312>: {2}\n<:Secret_Coin:1166362293660025064>: {3}\n<:Silver_Coin:1166362296159834202>: {4}\n<:Creator_Point:1169589714110644295>: {5}".format(stats.stars, stats.diamonds, stats.demons, stats.secret_coins, stats.user_coins, stats.creator_points))
+        .add_field(name="Most Recent Level", value="{0} ({1})\n<:Share:1166362299179745422>: {2}".format(name, id, downloads))
 
     )
     await interaction.response.send_message(embed=e)
 
+@search_group.command(name="level", description="Search for a level")
+async def search_level(interaction, level : str):
+    try:
+        search = await gd_client.search_levels(level)
+    except:
+        await interaction.response.send_message("There was an issue finding the level.")
+    
+    if search:
+        level = search[0]
+        await interaction.response.defer()
+        await asyncio.sleep(1)
+
+        ld_name = str.replace(level.difficulty.name, '_', ' ').lower().split()
+        ld_name = list(map(str.capitalize, ld_name))
+        ld_name = "{0} {1}".format(ld_name[0], ld_name[1])
+
+        song_author = level.song.artist
+        
+        e = (
+            discord.Embed(colour=0xFF1E27)
+            .add_field(name="Name", value=level.name)
+            .add_field(name="Rating", value="{0}<:Star:1166360223859101737> ({1})".format(level.stars, ld_name))
+            .add_field(name="Stats", value="<:Share:1166362299179745422>: {0}\n<:Fake_Spike:1169611005098205204>: {1}".format(level.downloads, level.object_count), inline=False)
+            .add_field(name="Song", value="{0} by {1} ([Link]({2}))".format(level.song.name, song_author, level.song.url), inline=False)
+        )
+
+        await interaction.followup.send(embed=e)
+
+t.add_command(search_group)
 client.run(yagddb.config["token"])
 
