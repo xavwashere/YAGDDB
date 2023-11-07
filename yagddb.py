ff83266a8a8ccdfb3b13a81f61dc790dc3906e15
Exyrus <97399129+Exyrus@users.noreply.github.com>
2023-11-07 16:29:44 +0000

    fairly large update that i forgot to commit :/

--- yagddb.py
+++ yagddb.py
@@ -16,7 +16,11 @@ import asyncio
 import requests
 import os
 from typing import Literal
+from typing import Union
 import httpx
+import yt_dlp
+from youtube_search import YoutubeSearch
+
 # get the starting time
 start = time.perf_counter()
 
@@ -87,9 +91,9 @@ class MusicBtns(discord.ui.View):
         vc = discord.utils.get(client.voice_clients, guild=guild)
         if vc.is_connected():
             await vc.disconnect()
-            await interaction.response.send_message("Disconnected from VC.")
+            await interaction.response.send_message("Disconnected from VC.", ephemeral=True)
         else:
-            await interaction.response.send_message("The bot isn't playing music right now!")
+            await interaction.response.send_message("The bot isn't playing music right now!", ephemeral=True)
 @gd_client.event
 async def on_rate(level : gd.Level):
     p = []
@@ -99,6 +103,25 @@ async def on_rate(level : gd.Level):
         w = discord.Webhook.from_url(x)
         w.send(content="New rated level!", embed=create_level_embed(level), username="Geometry Dash", avatar_url="https://upload.wikimedia.org/wikipedia/en/3/35/Geometry_Dash_Logo.PNG")
         
+async def _get_media(interaction : discord.Interaction, message : discord.Message):
+    user = message.author
+    if not user:
+        await interaction.response.send_message("User not found? Are you running this on a webhook?")
+    other = ""
+    media_found = 0
+    profile = user.display_avatar
+    attachments = []
+    if profile:
+        url = profile._url
+        media_found += 1
+    for m in message.attachments:
+        attachments.append(m)
+        media_found += 1
+    s = "s" if media_found > 1 else ""
+    for a in attachments:
+        other += "\n\n[{0}]({1})".format(a.filename, a.url)
+    message = "{0} attachment{1} in message.\n\n[Profile Picture]({2}){3}".format(media_found, s, url, other)
+    await interaction.response.send_message(message, suppress_embeds=True)
 
 async def get_demon_list(limit : int = 10) -> dict:
     pointercrate = "https://pointercrate.com/api/v2/"
@@ -116,7 +139,20 @@ async def get_player_list(limit : int = 10) -> dict:
     # r = json.loads(res)
     return res
 
-def create_level_embed(level : gd.Level, extra_info : dict or None = {"title": "Level: ", "thumbnail": None}) -> discord.Embed:
+def get_amount_of_players():
+    res = requests.get("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=322170").json()
+    if res["response"]["result"] != 1:
+        return (
+            discord.Embed(colour=0xC9FF, title="Current amount of people playing Geometry Dash on Steam")
+            .add_field(name="", value="There was an issue while sending the request to the Steam API. Try again later.")
+        )
+    else:
+        return (
+            discord.Embed(colour=0xC9FF, title="Current amount of people playing Geometry Dash on Steam")
+            .add_field(name="", value="{:,} people are currently playing GD on Steam.".format(res["response"]["player_count"]))
+        )
+
+def create_level_embed(level : gd.Level, extra_info : Union[dict, None] = {"title": "Level: ", "thumbnail": None}) -> discord.Embed:
     ld_name = str.replace(level.difficulty.name, '_', ' ').lower().split()
     ld_name = list(map(str.capitalize, ld_name))
     ld_name = "{0} {1}".format(ld_name[0], ld_name[1]) if len(ld_name) > 1 else ld_name[0]
@@ -163,6 +199,19 @@ def create_level_embed(level : gd.Level, extra_info : dict or None = {"title": "
     gameplay_type = "One-Player"
     if level.two_player:
         gameplay_type = "Two-Player"
+    
+    size = level.song.size
+
+    url = "[{0} on Newgrounds]({1})".format(level.song.name, level.song.url)
+    
+    download = "[Download {0} as MP3]({1})".format(level.song.name, level.song.download_url)
+
+    if not level.song.is_custom():
+        url = "Song is a default song, no link available."
+        download = "Song is a default song, no download available."
+    
+    if size is None:
+        size = "Unknown"
 
     
 
@@ -171,8 +220,8 @@ def create_level_embed(level : gd.Level, extra_info : dict or None = {"title": "
         .add_field(name="", value="**Description**: {0}\n\n**Coins**: {1}".format(level.description, coins))
         .add_field(name="Rating", value="{0}<:Star:1166360223859101737> ({1})".format(level.stars, ld_name), inline=False)
         .add_field(name="Stats", value="<:Share:1166362299179745422> {0}\n{1} {2}\n<:Fake_Spike:1169611005098205204> {3}\n<:Length:1170332753066197032>  {4}".format(level.downloads, like_type, level.rating, level.object_count, length), inline=False)
-        .add_field(name="Song", value="{0} by {1} ({2} MB)\n<:Play:1170315572261703830> [{3} on Newgrounds]({4})\n<:Download:1170329573448220802> [Download {5} as MP3]({6})".format(level.song.name, song_author, level.song.size, level.song.name, level.song.url, level.song.name, level.song.download_url), inline=False)
-        .add_field(name="Other Stats", value="ID: {0}\nCopyable: {1}\nGameplay Type: {2}\nCopied: {3}".format(level.id, copyable, gameplay_type, copied))
+        .add_field(name="Song", value="{0} by {1} ({2} MB)\n<:Play:1170315572261703830> {3}\n<:Download:1170329573448220802> {4}".format(level.song.name, song_author, size, url, download), inline=False)
+        .add_field(name="Other Stats", value="ID: {0}\nCopyable: {1}\nGameplay Type: {2}\nCopied: {3}\n<:Secret_Coin:1166362293660025064> [GD Browser Link](https://gdbrowser.com/{4})".format(level.id, copyable, gameplay_type, copied, level.id))
         .set_footer(icon_url="https://preview.redd.it/putting-my-geometry-dash-creator-points-image-here-so-v0-sgfl38xxycta1.png?width=640&crop=smart&auto=webp&s=817ca45d6616a201980b7be4fd980ec53e26f721", text="{0}".format(level.creator.name))
     )
     if extra_info["thumbnail"] is not None:
@@ -225,6 +274,8 @@ async def get_owner(guild_id : int) -> discord.Member:
     return guild.owner
 
 search_group = app_commands.Group(name="search", description="Search for something (users or levels)")
+music_group = app_commands.Group(name="music", description="Play music from YouTube or Newgrounds")
+server_group = app_commands.Group(name="server", description="Commands related to the current Discord server.")
 
 # uwu
 @t.command(name="ping")
@@ -475,12 +526,13 @@ async def settings(interaction):
     await interaction.response.send_message(embed=e, view=SettingsBtns())
 
 
-@t.command(name="music", description="Play a song from Newgrounds.")
-async def music(interaction : discord.Interaction, id : int):
+@music_group.command(name="newgrounds", description="Play a song from Newgrounds.")
+async def newgrounds(interaction : discord.Interaction, id : int):
     try:
         channel = interaction.user.voice.channel
     except:
         await interaction.response.send_message("You are not in a voice channel! Join one before continuing!")
+        return
     try:
         music = await gd_client.get_song(id)
     except:
@@ -505,8 +557,60 @@ async def music(interaction : discord.Interaction, id : int):
         await asyncio.sleep(1)
     await cv.disconnect()
 
-@t.command(name="randomlevel", description="Get a random level with the specified difficulty.")
+# @music_group.command(name="youtube", description="Play a song from YouTube.")
+# async def youtube(interaction, name : str):
+#     try:
+#         channel = interaction.user.voice.channel
+#     except:
+#         await interaction.response.send_message("You are not in a voice channel! Join one before continuing!", ephemeral=True)
+#         return
+#     await interaction.response.send_message("Getting info of {0}.".format(name), ephemeral=True)
+    
+#     res = YoutubeSearch(name, max_results=1).to_dict()
 
+#     if res:
+#         id = res[0]["id"]
+#         url = "https://www.youtube.com/watch?v={0}".format(id)
+
+        
+#         ytdl_opts = {
+#             "format": "bestaudio/best",
+#             "postprocessors": [{
+#                 "key": "FFmpegExtractAudio",
+#                 "preferredcodec": "mp3",
+#                 "preferredquality": "192",
+#             }],
+#             "postprocessor_args": [
+#                 "-ar", "44100", "-ac", "2"
+#             ],
+#             "prefer_ffmpeg": True,
+#             "keepvideo": False
+#         }
+
+#         ffmpeg_opts = {
+#             "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+#             "options": "-vn -filter:a \"volume=0.25\""
+#         }
+
+#         with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
+#             info = ytdl.extract_info(url, download=False)
+#             download = info["url"]
+#         print(download)
+#         cv = await channel.connect()
+#         await interaction.followup.send("Joined voice channel! Controls:", view=MusicBtns(), ephemeral=True)
+#         cv.play(discord.FFmpegPCMAudio(download, **ffmpeg_opts))
+#         cv.is_playing()
+
+#         while cv.is_connected():
+#             if len(channel.members) < 2:
+#                 await cv.disconnect()
+#             await asyncio.sleep(1)
+#         await cv.disconnect()
+
+
+    
+
+@t.command(name="randomlevel", description="Get a random level with the specified difficulty.")
 async def random_level(interaction, difficulty: Literal["Auto", "Easy", "Normal", "Hard", "Harder", "Insane", "Easy Demon", "Medium Demon", "Hard Demon", "Insane Demon", "Extreme Demon"]):
     await interaction.response.defer()
     await asyncio.sleep(0.1)
@@ -522,8 +626,30 @@ async def random_level(interaction, difficulty: Literal["Auto", "Easy", "Normal"
     e = create_level_embed(level, {"title": "Random Level: ", "thumbnail": None})
     await interaction.followup.send(embed=e)
 
+@server_group.command(name="members", description="See how many members the server has")
+async def members(interaction):
+    guild = interaction.guild
+    member_count = len(guild.members)
+    e = (
+        discord.Embed(colour=0x00C9FF, title=guild.name)
+        .add_field(name="", value="**Members: {0}**".format(member_count))
+    )
 
+    await interaction.response.send_message(embed=e)
+
+@t.command(name="currentplayers", description="Get the current number of people playing Geometry Dash on Steam")
+async def current_players(interaction):
+    await interaction.response.send_message(embed=get_amount_of_players())
+
+get_media = app_commands.ContextMenu(
+    name="Get Media from Message",
+    callback=_get_media
+)
+
+t.add_command(get_media)
 t.add_command(search_group)
+t.add_command(music_group)
+t.add_command(server_group)
 try:
     client.run(yagddb.config["token"])
 finally:
